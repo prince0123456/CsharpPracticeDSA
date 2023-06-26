d0a0d2de9a0559537e17b89835d3c983c3428b34
Prince Gupta <princegupta.pg46@gmail.com>
2023-06-26 15:43:17 +0530

    Exception Handling Practice

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static double DOdivide(double x,double y)
+        {
+            if (y== 0) 
+            {
+                throw new System.DivideByZeroException();   
+            }
+            return x / y;
+        }
+        static void Main()
+        {
+            double num1 = 5;
+            double num2 = 0;
+            try
+            {
+                Console.WriteLine("5/0 ={0}",DOdivide(num1,num2));
+            }catch(DivideByZeroException ex) 
+            {
+                Console.WriteLine("cant divide by zero ");
+                Console.WriteLine(ex.Message);
+            }
+            finally { Console.WriteLine("Cleaning block"); }
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+
+            int start = 0;
+            int end = charArray.Length - 1;
+
+            while (start < end)
+            {
+                // Swap characters
+                char temp = charArray[start];
+                charArray[start] = charArray[end];
+                charArray[end] = temp;
+
+                start++;
+                end--;
+            }
+
+            return new string(charArray);
+        }
+    }
+}
---
+++ ConsoleApp1/ReverseStr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ReverseStr { 
+        public static void revsestr()
+        {
+            string[] arr = { "Prince", "Gupta" };
+
+            // Reverse each string in the array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = ReverseString(arr[i]);
+            }
+
+            // Output the reversed array
+            foreach (string str in arr)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charArray = new char[str.Length];
+
+            // Reverse the characters in the string
+            for (int i = 0, j = str.Length - 1; i < str.Length; i++, j--)
+            {
+                charArray[i] = str[j];
+            }
+
+            return new string(charArray);
+        }
+    }
+
+}
